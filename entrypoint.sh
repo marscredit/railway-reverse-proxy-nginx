9b2d8ccc334339a5d133b86e4742dc390e29ee53
Jeremy Crane <jeremyccrane@me.com>
2024-06-12 22:06:55 -0600

    . [

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,5 +11,11 @@ write_red() {
   echo ${_RED}${1}${_RESET}
 }
 
+# Check if PROXY_HOST is valid
+if [[ $PROXY_HOST != *.railway.internal ]]; then
+    write_red "Not a valid host: $PROXY_HOST"
+    exit 0
+fi
+
 # Start Nginx
-nginx -g "daemon off;"
+nginx -g 'daemon off;'
