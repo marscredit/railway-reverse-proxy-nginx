437e905678a26c7517e71bc9768c5767f84ba987
Jeremy Crane <jeremyccrane@me.com>
2024-06-12 21:56:35 -0600

    updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-echo "Starting Nginx"
+set -e
 
-# Replace placeholders with environment variables
-envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+export TERM=ansi
+
+_RED=$(tput setaf 1)
+_RESET=$(tput sgr0)
+
+write_red() {
+  echo ${_RED}${1}${_RESET}
+}
 
 # Start Nginx
-nginx -g 'daemon off;'
+nginx -g "daemon off;"
